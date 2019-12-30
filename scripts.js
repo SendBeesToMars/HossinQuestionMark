c6241f40a2e0e572cfcab4a4d21281d44177a58f
Winmillion <datum1337@gmail.com>
2019-12-30 03:51:58 +0000

    todo: hossin viability row

--- scripts.js
+++ scripts.js
@@ -24,42 +24,72 @@ function build_table(){
     getServers(CORS_bypass + servers_URL, CORS_bypass + server_info_URL)
     .then(([servers, server_info]) =>{
 
-        console.log("wtf is this bullshit");
         servers = servers.world_list;
-        
-        // servers.forEach(server => {
-        // if(server.name.en != "Briggs"){
-        //     server_pop = (server_info["result"][server["world_id"]][0]["nc"] 
-        //             + server_info["result"][server["world_id"]][0]["tr"] 
-        //             + server_info["result"][server["world_id"]][0]["vs"] 
-        //             + server_info["result"][server["world_id"]][0]["ns"]);
-        // }
-        // if(server.name.en != "Briggs"){
-        //     table_contents += ("<tr>" + "<td>" + server.name.en + "</td>" + "<td>" + server.world_id + "</td>" + "</tr>");
-        // }
-        // table_contents += "</table>";
-        // table_div.innerHTML = table_contents;
-        // });
+
+        let server_pop = 0;
+        servers.forEach(server => {
+            hossin_viability = true;
+            if(server.name.en != "Briggs"){
+                server_pop = (server_info["result"][server["world_id"]][0]["nc"] 
+                        + server_info["result"][server["world_id"]][0]["tr"] 
+                        + server_info["result"][server["world_id"]][0]["vs"] 
+                        + server_info["result"][server["world_id"]][0]["ns"]);
+                if (server_pop > 600){
+                    let server_terry_caps;
+                    while(typeof server_terry_caps === "undefined"){
+                        try{
+                            server_terry_caps = fetch_data_from_URL(CORS_bypass + fisu_terry_URL + "/?world=" + [server["world_id"]][0] + "&continent=4");
+                        }
+                        catch(err){
+                            console.log("shits fucked for " + server["name"]["en"] + "... trying again " + err);
+                            sleep(15000);
+                        }
+                    }
+                    
+                    server_terry_caps.then((res) => {
+                        console.log(res.result[0].continents[0].control);
+                        continent_terrys = res.result[0].continents[0].control;
+                        continent_terrys.forEach(cap => {
+                            console.log(cap);
+                            if(parseInt(cap) > 40){
+                                console.log("setting hossing to false");
+                                hossin_viability = false;
+                            }
+                        })
+                    });
+                }
+                else {
+                    hossin_viability = false;
+                }
+            }
+            
+            if(server.name.en != "Briggs"){
+                table_contents += ("<tr>" + "<td>" + server.name.en + "</td>" + "<td>" + server.world_id + "</td>" + "<td>" + server_pop + "</td>" + "<td>" + hossin_viability + "</td>" + "</tr>");
+            }
+        });
+
+    table_contents += "</table>";
+    table_div.innerHTML = table_contents;
     });
 }
 
 function fetch_data_from_URL(url){
-    fetch(url, {
+    return fetch(url, {
         headers: {
             'Content-Type': 'application/json',
             'Accept': 'application/json'
         }
     })
     .then(res => res.json())
-    .then((out) => {
-        console.log(out);
-        return out;
-    })
     .catch(err => {throw err});
 }
 
 function getServers(url, url2){
     return Promise.all([fetch_data_from_URL(url), fetch_data_from_URL(url2)]);
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 build_table();
